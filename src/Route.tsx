abe441bee5c63e94bd6aa033177169b701941a57
vengadeshraj <vengadeshraj405@gmail.com>
2026-04-07 23:21:38 +0530

    base setup added

---
+++ src/Route.tsx
@@ -0,0 +1,9 @@
+const Route = () => {
+  return (
+    <div>
+      <div>Route Page</div>
+    </div>
+  );
+};
+
+export default Route;
---
+++ src/pages/Chat.tsx
@@ -0,0 +1,9 @@
+const Chat = () => {
+  return (
+    <div>
+      <div>Chat Page</div>
+    </div>
+  );
+};
+
+export default Chat;
