aedb08e7846dcd03d0f5b190aa4ed94f42302ddc
vengadeshraj <vengadeshraj405@gmail.com>
2026-04-22 20:32:39 +0530

    default path handled

--- src/WavvyRoutes.tsx
+++ src/WavvyRoutes.tsx
@@ -1,14 +1,14 @@
-import { BrowserRouter, Routes, Route  } from 'react-router-dom';
-import Login from './pages/Login';
-import Chat from './pages/Chat';
-
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Chat from "./pages/Chat";
 
 const WavvyRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
-          <Route path="/log-in" element={<Login/>} />
-          <Route path="/chat" element={<Chat/>} />
+        <Route path="/" element={<Navigate to="/log-in" replace />} />
+        <Route path="/log-in" element={<Login />} />
+        <Route path="/chat" element={<Chat />} />
       </Routes>
     </BrowserRouter>
   );
